639912aecde5ef77723589c579984fef1050e49b
Jason Kim <34352746+jasonminsookim@users.noreply.github.com>
2025-04-08 21:24:09 -0700

    use chordal hold rather than achordian

--- config.h
+++ config.h
@@ -52,6 +52,7 @@
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 100
 
 // Pick good defaults for enabling homerow modifiers
+#define CHORDAL_HOLD
 #define TAPPING_TERM 175 
 #define PERMISSIVE_HOLD
 #define QUICK_TAP_TERM_PER_KEY
