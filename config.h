1e2c3b2f0c71386a1fb13e1b8a0655d7d29c2e90
Jason Kim <34352746+jasonminsookim@users.noreply.github.com>
2025-04-10 12:03:09 -0700

    modify tapping term

--- config.h
+++ config.h
@@ -52,8 +52,8 @@
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 100
 
 // Pick good defaults for enabling homerow modifiers
+#define TAPPING_TERM 200
 #define CHORDAL_HOLD
-#define TAPPING_TERM 125 
 #define PERMISSIVE_HOLD
 #define QUICK_TAP_TERM_PER_KEY
 #define USB_VBUS_PIN 19
